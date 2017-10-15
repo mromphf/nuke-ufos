48a10fd0acb07805137e56564344e530e1ad8d40
Mark Romphf <markromphf@gmail.com>
2017-10-14 19:38:24 -0600

    Delegate player operations to the player function.

--- game.js
+++ game.js
@@ -2,35 +2,7 @@ var game = (function() {
     var maxWidth = window.innerWidth;
     var maxHeight = window.innerHeight;
     var stars = [];
-
-    var player = {
-        x: 0,
-        y: 0,
-
-        moveLeft: function() {
-            if ((this.x - 20) > 0) {
-                this.x = this.x - 20;
-            }
-        },
-
-        moveUp: function() {
-            if ((this.y - 20) > 0) {
-                this.y = this.y - 20;
-            }
-        },
-
-        moveRight: function() {
-            if ((this.x + 20) < maxWidth) {
-                this.x = this.x + 20;
-            }
-        },
-
-        moveDown: function() {
-            if ((this.y + 20) < maxHeight) {
-                this.y = this.y + 20;
-            }
-        }
-    };
+    var ply = new player(maxWidth, maxHeight);
 
     function randomStarsAnywhere(numExistingStars) {
         var result = [];
@@ -60,15 +32,13 @@ var game = (function() {
     }
 
     var initialize = function() {
-        this.player.x = maxWidth / 2;
-        this.player.y = maxHeight - (maxHeight * 0.2);
         this.stars = randomStarsAnywhere(0);
     }
 
     return {
         initialize: initialize,
         moveStars: moveStars,
-        player: player,
+        player: ply,
         stars: stars
     };
 })();
--- screen.js
+++ screen.js
@@ -1,19 +1,12 @@
 var screen = (function() {
     var canvas = document.getElementById("screen");
     var context = canvas.getContext("2d");
-
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
     function drawBackground() {
         context.fillStyle = "#020202";
         context.fillRect(0, 0, canvas.width, canvas.height);
-    }
-
-    function drawPlayer(player) {
-        context.beginPath();
-        context.arc(player.x, player.y, 30, 0, 2 * Math.PI);
-        context.fillStyle = "#00f";
     }
 
     function drawStars(stars) {
@@ -28,7 +21,7 @@ var screen = (function() {
     function render(player, stars) {
         drawBackground();
         drawStars(stars);
-        drawPlayer(player);
+        player.draw(context);
         context.fill();
     }
 
