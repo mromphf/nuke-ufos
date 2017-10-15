244746e167feee37983759246e44cc2be2d36de0
Mark Romphf <markromphf@gmail.com>
2017-10-14 20:23:33 -0600

    Don't need to bind the x and y properties on the star function.

--- star.js
+++ star.js
@@ -3,23 +3,21 @@ var Star = function(x, y) {
     var y = y;
 
     function isStillAbove(maxScreenHeight) {
-        return (this.y + 2) < maxScreenHeight;
+        return (y + 2) < maxScreenHeight;
     }
 
     function move() {
-        this.y = this.y + 2;
+        y = y + 2;
     }
 
     function draw(context) {
         context.beginPath();
-        context.arc(this.x, this.y, 2, 0, 2 * Math.PI);
+        context.arc(x, y, 2, 0, 2 * Math.PI);
         context.fillStyle = "#eee";
         context.fill();
     }
 
     return {
-        x: x,
-        y: y,
         isStillAbove: isStillAbove,
         move: move,
         draw: draw
