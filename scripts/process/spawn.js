c41b4c3abe5dfc909b32b59e496fcac509382404
Mark Romphf <markromphf@gmail.com>
2019-03-30 08:10:57 -0600

    Advanced spawn rules.

--- scripts/process/spawn.js
+++ scripts/process/spawn.js
@@ -5,6 +5,42 @@ let spawn = (function() {
         return Math.floor((Math.random() * screen.WIDTH) + 1);
     }
 
+    function delayBetweenSpawns(elapsedTime) {
+        if (elapsedTime < 15000) {
+            return 2000;
+        }
+        else if (elapsedTime < 30000) {
+            return 1500;
+        }
+        else if (elapsedTime < 30000) {
+            return 1000;
+        }
+        else if (elapsedTime < 60000) {
+            return 500;
+        }
+        return 100;
+    }
+
+    function maximumEnemies(elapsedTime) {
+        if (elapsedTime < 10000) {
+            return 1;
+        }
+        else if (elapsedTime < 20000) {
+            return 3;
+        }
+        else if (elapsedTime < 30000) {
+            return 4;
+        }
+        else if (elapsedTime < 60000) {
+            return 5;
+        }
+        else if (elapsedTime < 120000) {
+            return 7;
+        }
+        return 10;
+    }
+
+
     function generateRandomEnemy(elapsedTime) {
         if (Math.random() > 0.5) {
             return new Kamikaze(randomTop(screen.WIDTH), 0, elapsedTime);
@@ -26,8 +62,8 @@ let spawn = (function() {
 
     function randomEnemy(actors, elapsedTime) {
         if (elapsedTime > 3000
-            && (elapsedTime - lastSpawnTime(actors.filter(a => a.isEnemy)) > 2000)
-            && actors.filter(a => a.isEnemy).length < 3) {
+            && (elapsedTime - lastSpawnTime(actors.filter(a => a.isEnemy)) > delayBetweenSpawns(elapsedTime))
+            && actors.filter(a => a.isEnemy).length < maximumEnemies(elapsedTime)) {
                 actors.push(generateRandomEnemy(elapsedTime));
         }
         return actors;
