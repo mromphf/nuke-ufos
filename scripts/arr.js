869eaaa9aa2d1c1fedf8be68042921e91a9c21cd
Mark Romphf <markromphf@gmail.com>
2019-04-11 05:13:17 -0600

    Add immutable push function.

--- scripts/arr.js
+++ scripts/arr.js
@@ -3,5 +3,11 @@
 let arr = {
     randomElement: function(arr) {
         return arr[(Math.floor((Math.random() * arr.length)) + 1) - 1];
+    },
+
+    push: function(arr, elm) {
+        let a = arr;
+        a.push(elm);
+        return a;
     }
 }
