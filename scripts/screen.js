42796590e3d2edf1f90bb3f3bf0ea6c3cc69df9b
Mark Romphf <markromphf@gmail.com>
2019-04-12 05:23:33 -0600

    Max resolution.

--- scripts/screen.js
+++ scripts/screen.js
@@ -1,8 +1,8 @@
 "use strict";
 
 let screen = (function() {
-    const WIDTH = window.innerWidth;
-    const HEIGHT = window.innerHeight;
+    const WIDTH = Math.max(1920, window.outerWidth);
+    const HEIGHT = Math.max(1080, window.outerHeight);
     let canvasA = document.getElementById("hud");
     let canvasB = document.getElementById("foreground");
     let canvasC = document.getElementById("background");
