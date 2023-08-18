8c4e004b8b9b0279263e2c458f4409956aeb6629
Mark Romphf <markromphf@gmail.com>
2023-08-17 19:18:50 -0600

    Despawn out-of-bounds ufo mobs.

--- scripts/ufo.gd
+++ scripts/ufo.gd
@@ -1,11 +1,16 @@
 extends Enemy
 
+var _VELOCITY: Vector2 = Vector2(0, 2)
+
 func _despawn():
 	queue_free()
 
 func _process(_delta):
 	rotate(PI / 180)
-	position += Vector2(0, 2)
+	position += _VELOCITY
+	if position.y >= 1005:
+		print("UFO despawned")
+		queue_free()
 
 func _on_collide(_body):
 	if _body is Laser:
