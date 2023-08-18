b229ddf210a1093b2a0f9d3f6a2b0193f45ef22e
Mark Romphf <markromphf@gmail.com>
2023-08-17 19:37:22 -0600

    Generate stars instead of hard-coding in scene.

--- scripts/background.gd
+++ scripts/background.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var star_scene: PackedScene
+var _NUM_STARS = 20
 
 func _tick():
 	$StarPath/StarSpawn.progress = randi()
@@ -9,3 +10,14 @@ func _tick():
 	star.position = $StarPath/StarSpawn.position
 
 	add_child(star)
+
+
+func _ready():
+	var screen_size = get_viewport_rect().size
+
+	for x in range(0, _NUM_STARS):
+		var star = star_scene.instantiate()
+		star.position = Vector2( \
+			randf_range(0, screen_size.x), \
+			randf_range(0, screen_size.y))
+		add_child(star)
