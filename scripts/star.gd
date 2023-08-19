926ca8b5fb7b1371e60d56d3fb0ac713a9f2e383
Mark Romphf <markromphf@gmail.com>
2023-08-19 16:26:21 -0600

    Use viewport size in star script.

--- scripts/star.gd
+++ scripts/star.gd
@@ -1,9 +1,11 @@
 extends Sprite2D
 
+@onready var _BOUND = get_viewport_rect().size.y
+
 var _VELOCITY = Vector2(0.0, 1.0)
 
 func _process(delta):
 	position += _VELOCITY
-	if position.y >= 1005:
+	if position.y > _BOUND:
 		queue_free()
 
