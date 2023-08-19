6d663d2a40298a571fce51477be6c22c07d1a5d2
Mark Romphf <markromphf@gmail.com>
2023-08-19 17:38:50 -0600

    pop enemies

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+class_name Player
+
 signal hit
 signal burn
 
--- scripts/ufo.gd
+++ scripts/ufo.gd
@@ -15,16 +15,21 @@ func _process(_delta):
 	rotate(PI / 180)
 
 
-func _on_collide(_body):
+func _die():
+	var d = death.instantiate()
+	d.position = position
+	get_parent().add_child(d)
+	emit_signal(&"dead", 10)
+	queue_free()
+
+func _on_collide(body):
 	_hp -= 1
+
+	if body is Player or _hp <= 0:
+		_die()
+
 	$Animation.play(&"flash")
 	$Hit.play()
-	if _hp <= 0:
-		var d = death.instantiate()
-		d.position = position
-		get_parent().add_child(d)
-		emit_signal(&"dead", 10)
-		queue_free()
 
 
 func _ready():
