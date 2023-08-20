3d26113ffdc1e303eb97f4a92224d7d29db491e5
Mark Romphf <markromphf@gmail.com>
2023-08-20 11:32:43 -0600

    Correct star velocity and direction.

--- scripts/star.gd
+++ scripts/star.gd
@@ -1,9 +1,10 @@
 extends Sprite2D
 
-var _VELOCITY = Vector2(0.0, 1.0)
+var _speed = 1
+var _direction = Vector2(0.0, 1.0)
 
 func despawn():
 	queue_free()
 
 func _process(_delta):
-	position += _VELOCITY
+	position += (_direction.normalized() * _speed)
