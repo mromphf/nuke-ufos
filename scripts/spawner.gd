7ba32a6417b92b4a47d574b4c3e1c9b41fd9bbc6
Mark Romphf <markromphf@gmail.com>
2023-08-20 14:13:38 -0600

    Progressive spawn pool.

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -2,17 +2,42 @@ extends Node2D
 
 class_name Spawner
 
-var spawn_pool = [
-	preload("res://scenes/ufo.tscn"),
-	preload("res://scenes/dark_ufo.tscn"),
-	preload("res://scenes/striker.tscn"),
-]
+var _bound_left = 0.35
+var _bound_right = 0.65
+
+var ufo = preload("res://scenes/ufo.tscn")
+var dark_ufo = preload("res://scenes/dark_ufo.tscn")
+var striker = preload("res://scenes/striker.tscn")
+
+var spawn_pool = [ufo]
+
+
+func _phase3():
+	_bound_left = 0
+	_bound_right = 1
+	spawn_pool.push_back(dark_ufo)
+	$Timer.wait_time = 1.5
+
+
+func _phase2():
+	_bound_left = 0.25
+	_bound_right = 0.75
+	spawn_pool.push_back(striker)
+	$Phase3.start()
+
+
+func _range():
+	return randf_range(_bound_left, _bound_right)
+
 
 func _tick():
-	$Spawn/SpawnPoint.progress_ratio = randf_range(0.25, 0.75)
+	$Spawn/SpawnPoint.progress_ratio = _range()
 	
 	var mob = spawn_pool.pick_random().instantiate()
 	mob.position = $Spawn/SpawnPoint.position
 	
 	mob.connect(&"dead", get_parent().get_node("hud").update_score)
 	add_child(mob)
+
+func _ready():
+	$Phase2.start()
